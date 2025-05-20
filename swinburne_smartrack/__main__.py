14fd9d16799907f2dc9747d945526c02f72be91d
jason <jbut@swin.edu.au>
2025-05-21 01:52:54 +1000

    1) SmartRack test code moved to __main__.py
    2) Clean up smartrack.py to remove test code and remove UI
    
    3) UI no longer in SmartRack class, in main application to keep class to only access website

--- swinburne_smartrack/__main__.py
+++ swinburne_smartrack/__main__.py
@@ -7,11 +7,12 @@ import argparse
 # requests     - HTTP/HTTPS Client
 import re
 import logging
+
+import dialog
+
 from .config import Configuration
 from .smartrack import SmartRack
 import rich.console
-#import dialog
-#import requests
 from rich.table import Table
 from rich.panel import Panel
 
@@ -20,9 +21,78 @@ from rich.panel import Panel
 # TODO: Re-write smartrack test command to extract UI from SmartRack class
 # TODO: Think about how logging is handled in this file
 
-def smartrack(arguments: argparse.Namespace):
+
+def smartrack(arguments: argparse.Namespace, console: rich.console) -> None:
+    def confirm_termination(title: str) -> None:
+        """
+        Ask the user if they wish to terminate the test suite, if so print message to screen and terminate, otherwise return.
+
+        :param title: Namespace object containing parsed command-line
+                          arguments for the application.
+        :param console: Rich console instance used to format and display output.
+        :return: None
+        """
+        if __dialog.yesno('Are you sure that you want to terminate application?', title=title) == __dialog.OK:
+            console.clear()
+            console.print(Panel('Terminating SmartRack Web Site Test Suite', style='bold red'))
+            exit(0)
+
+    def select_servers(title: str, instructions: str) -> list[str]:
+        """
+        Select one or more servers from a checklist dialog.
+
+        This function presents a checklist dialog box to the user, allowing them to select one or more servers from the list in the
+        system SmartRack configuration file. It loops until the user makes a valid selection or confirms quitting. If the user selects OK, but no
+        servers are chosen, an error message is displayed, prompting them to try again.
+
+        :param title: The title to display at the top of the checklist dialog.
+        :param instructions: Instructions displayed to the user in the checklist dialog.
+        :return: A list of server identifiers chosen by the user.
+        """
+        # Loop forever asking user to select room, when one or more rooms are selected, break out of loop
+        while True:
+            # Display message box
+            code, selected_rooms = __dialog.checklist(instructions,
+                                                      choices=[(key, value['description'], False) for key, value in Configuration().smartrack_servers.items()],
+                                                      title=title,
+                                                      cancel_label='Quit'
+                                                      )
+
+            if code == __dialog.OK:
+                # User selected OK, if at least one room is selected break out of loop, otherwise display error message and try again
+                if len(selected_rooms) > 0: return selected_rooms
+                __dialog.msgbox('ERROR: You must select at least one room', title=title)
+            else:
+                # User selected QUIT, confirm termination (will raise exception if user confirms, otherwise continue and try again)
+                confirm_termination(title)
+
+    def ask_auth_details(title: str) -> dict[str, str]:
+        """
+        Prompt the user to input authentication details for SmartRack. The function displays a dialog box for the user to input a username
+        and password, the password field is hidden The dialog allows the user to quit. If "Quit" is selected, the function will
+        confirm termination of the test.
+
+        :param title: The title displayed on the authentication dialog box.
+        :return: A dictionary containing the username and password provided by the user, with keys `'username'` and `'password'` respectively.
+        """
+        while True:
+            # Display password entry box, each tuple in elements is:
+            #  field label, label y pos, label x pos, initial field value, field y pos, field x pos, field length, input length, 0=plaintext/1=hidden
+            code, values = __dialog.mixedform('Enter SmartRack Authentication details below:\n',
+                                              title=title,
+                                              elements=[("Username:", 2, 2, "", 2, 15, 50, 50, 0),
+                                                        ("Password:", 4, 2, "", 4, 15, 50, 50, 1)],
+                                              cancel_label='Quit',
+                                              insecure=True
+                                              )
+
+            if code == __dialog.OK: return {'username': values[0], 'password': values[1]}
+
+            # User selected QUIT, confirm termination (will raise exception if user confirms, otherwise continue and try again)
+            confirm_termination(title)
+
+    # Actual function starts here
     import rich.logging
-    console = rich.console.Console()
     logging.basicConfig(format='%(name)s.%(funcName)s() - %(message)s',
                         handlers=[rich.logging.RichHandler(markup=True, console=console)],
                         level=getattr(logging, arguments.debug)
@@ -30,21 +100,29 @@ def smartrack(arguments: argparse.Namespace):
 
     logger = logging.getLogger('')
 
-    config = Configuration()
-    # Create SmartRack instance
-    test = SmartRack(console, config.smartrack_servers)
+    __dialog = dialog.Dialog(dialog='dialog')
+    servers = select_servers(' ATC Room Selection ', 'Please select which rooms you would like to test this library with')
+    smartrack = SmartRack(console, Configuration().smartrack_servers)
 
-    # Ask user to select rooms
-    test.select_smartrack_rooms(' ATC Room Selection ', 'Please select which rooms you would like to test this library with')
+    while True:
+        try:
+            auth_details = ask_auth_details(' ATC Website Authentication Information ')
 
-    # Download booked devices
-    test.fetch_booked_devices(' ATC Website Authentication Information ')
+            console.clear()
+            console.print(Panel('SmartRack Web Site Test Suite', style='bold green'))
+            console.print()
+            console.rule('Retrieving Booked Devices from SmartRack Website')
+            smartrack.fetch_booked_devices(servers, auth_details)
+            break
+        except SmartRack.AuthError as e:
+            __dialog.msgbox(f'ERROR: {e}', title='Authentication Error')
 
     # Retrieve list (no filter, get all devices)
-    result = test.filter()
+    result = smartrack.filter()
 
     # Display all devices in table
-    logger.info('Displaying booked device details')
+    console.print()
+    console.rule('Displaying booked device details')
     from rich.table import Table
     table = Table(show_header=True, header_style="bold green", title="Booked Devices", show_lines=True)
     table.add_column("Room", style="green")
@@ -130,6 +208,10 @@ def ciscodevice(arguments: argparse.Namespace, console: rich.console) -> None:
     console.print(table)
 
 
+def devicemanager(arguments: argparse.Namespace, console: rich.console) -> None:
+    pass
+
+
 def parse_arguments() -> argparse.Namespace:
     # Create the main parser with global CLI parameters
     parser = argparse.ArgumentParser(description='Swinburne SmartRack Test Suite',
@@ -156,6 +238,7 @@ def parse_arguments() -> argparse.Namespace:
 
     subparsers.add_parser('smartrack', help='Test SmartRack website access', argument_default=smartrack).set_defaults(func=smartrack)
     subparsers.add_parser('ciscodevice', help='Test Cisco Device connection', parents=[connection_parser]).set_defaults(func=ciscodevice)
+    subparsers.add_parser('devicemanager', help='Test single device collection in sub-process', parents=[connection_parser]).set_defaults(func=devicemanager)
 
     return parser.parse_args()
 
@@ -168,7 +251,6 @@ if __name__ == '__main__':
 
         arguments.func(arguments, console)
 
-        print(arguments.debug)
     except KeyboardInterrupt as err:
         pass
     except (Exception,):
--- swinburne_smartrack/smartrack.py
+++ swinburne_smartrack/smartrack.py
@@ -19,7 +19,6 @@ Running the module directly will run a test suite allowing verification of funct
 import re
 import logging
 import rich.console
-import dialog
 import requests
 
 # Library modules
@@ -31,6 +30,9 @@ class SmartRack:
     """
     SmartRack class
     """
+    class AuthError(Exception):
+        pass
+
     def __init__(self, console: rich.console.Console, rooms: dict[str, dict[str, str]]):
         """
         Construct the SmartRack class instance.
@@ -48,84 +50,11 @@ class SmartRack:
 
         # Local variable for the console, also for the pythondialog instance
         self.__console = console
-        self.__dialog = dialog.Dialog(dialog='dialog')
-
-        # Store dictionary of SmartRack server details, and empty list of selected rooms
-        self.__rooms = rooms
-        self.__selected_rooms: list[str] = []
 
         # Initialise list holding all device connection details
         self.__devices: dict[str, dict[str, str]] = {}
 
-        # Initialise storage of SmartRack authentication details
-        self.__auth_details: dict[str, str] = {}
-
-    def _ask_to_terminate(self, title: str) -> None:
-        """
-        Ask the user whether they want to terminate the program or not
-          - If they choose YES, raise Exception to kill application
-          - If they choose NO, return and allow program to continue
-        :param title: Application title to display
-        """
-        self.__log.debug('Terminating Application')
-        if self.__dialog.yesno('Are you sure that you want to terminate application?', title=title) == self.__dialog.OK:
-            raise Exception('Terminating')
-
-    def _ask_auth_details(self, title: str) -> None:
-        """
-        Retrieve username/password to access the SmartRack site and save in self.__auth_details
-        :param title: Title to display at top of dialog box
-        :return:
-        """
-        while True:
-            # Display password entry box, each tuple in elements is:
-            #  field label, label y pos, label x pos, initial field value, field y pos, field x pos, field length, input length, 0=plaintext/1=hidden
-            code, values = self.__dialog.mixedform('Enter SmartRack Authentication details below:\n',
-                                                   title=title,
-                                                   elements=[("Username:", 2, 2, "", 2, 15, 50, 50, 0),
-                                                             ("Password:", 4, 2, "", 4, 15, 50, 50, 1)],
-                                                   cancel_label='Quit',
-                                                   insecure=True
-                                                   )
-
-            if code == self.__dialog.OK:
-                # Username and password provided, save and break out of loop
-                self.__auth_details = {'username': values[0], 'password': values[1]}
-                break
-
-            # User selected QUIT, confirm termination (will raise exception if user confirms, otherwise continue and try again)
-            self._ask_to_terminate(title)
-
-        self.__log.info(f'Authentication details: {self.__auth_details}')
-
-    def select_smartrack_rooms(self, title: str, instructions: str) -> None:
-        """
-        SelectRooms method
-        :return:
-        """
-        self.__log.debug(f'Ask user to select which rooms to use')
-
-        # Loop forever asking user to select room, when one or more rooms are selected, break out of loop
-        while True:
-            # Display message box
-            code, self.__selected_rooms = self.__dialog.checklist(instructions,
-                                                                  choices=[(key, value['description'], False) for key, value in self.__rooms.items()],
-                                                                  title=title,
-                                                                  cancel_label='Quit'
-                                                                  )
-
-            if code == self.__dialog.OK:
-                # User selected OK, if at least one room is selected break out of loop, otherwise display error message and try again
-                if len(self.__selected_rooms) > 0: break
-                self.__log.info(f'No rooms selected: asking again')
-                self.__dialog.msgbox('ERROR: You must select at least one room', title=title)
-            else:
-                # User selected QUIT, confirm termination (will raise exception if user confirms, otherwise continue and try again)
-                self._ask_to_terminate(title)
-
-        self.__log.info(f'Selected rooms: {self.__selected_rooms}')
-
-    def fetch_booked_devices(self, title: str) -> None:
+    def fetch_booked_devices(self, selected_rooms: list[str], auth_details: dict[str, str]) -> None:
         """
         Download all booked devices for all selected rooms, store all connection details in self.__devices.
 
@@ -135,27 +64,21 @@ class SmartRack:
 
         :param title: Title to display at top of dialog box requesting authentication information
         """
-        self._ask_auth_details(title)
-
-        self.__console.clear()
-
         with self.__console.status('[magenta]Downloading SmartRack booked devices', spinner='earth'):
-            for room in self.__selected_rooms:
-                url = self.__rooms[room]['url']
+            for room in selected_rooms:
+                url = Configuration().smartrack_servers[room]['url']
 
                 self.__console.print(f'Connecting to {room} at {url}')
                 self.__log.info(f'Attempting to connect to {room} at {url}')
 
-                r = requests.post(url, data=self.__auth_details)
+                r = requests.post(url, data=auth_details)
 
                 self.__log.info(f'HTTP status code: {r.status_code}')
                 if r.status_code != 200:
                     raise Exception(f'Unable to connect to {url}. Status code: {r.status_code}')
 
-                while r.content.decode('utf8') == 'Logon error\n':
-                    self.__dialog.msgbox('ERROR: Bad username/password combination supplied', title=f' Connecting to {room} ')
-                    self._ask_auth_details(title)
-                    r = requests.post(url, data=self.__auth_details)
+                if r.content.decode('utf8') == 'Logon error\n':
+                    raise SmartRack.AuthError('Bad username/password combination supplied')
 
                 split_response = r.content.decode('utf-8').splitlines()
                 self.__log.debug(f'Received device login information {split_response}')
@@ -192,7 +115,6 @@ class SmartRack:
 
     def filter(self, enclosures: list[str] = ['Black', 'Red', 'Blue', 'Green', 'Yellow'], kits: list[str] = ['Yellow', 'Green', 'Orange', 'Purple', 'White'], devices: list[str] = ['Switch 1', 'Switch 2', 'Switch 3', 'Switch 4', 'Router 1', 'Router 2', 'Router 3', 'Router 4']) -> dict[str, dict[str, str]]:
         """
-
         :param enclosures: List of strings of Enclosures we are interested in
         :param kits: List of strings of Kits we are interested in
         :param devices: List of strings of Devices we are interested in
@@ -208,66 +130,3 @@ class SmartRack:
         """
         self.__log.info(f'Filtering device list where nickname is one of {match}')
         return {key: value for key, value in self.__devices.items() if value['nickname'] in match}
-
-
-# Execute test/validation suite if run as python -m swinburne_smartrack.smartrack
-if __name__ == '__main__':
-    try:
-        # Create argparse instance and parse command line parameters
-        import argparse
-        parser = argparse.ArgumentParser(description='SmartRack Test Suite',
-                                         formatter_class=argparse.RawTextHelpFormatter,
-                                         allow_abbrev=False
-                                         )
-        parser.add_argument('-c', '--config-file',
-                            help='Specify the smartrack configuration file (default: system configuration)'
-                            )
-        parser.add_argument('-d', '--debug',
-                            default='INFO',
-                            choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-                            help='Set logging level (default: %(default)s)'
-                            )
-        arguments = parser.parse_args()
-
-        # Create the Rich Console and Rich Logger
-        import rich.logging
-        console = rich.console.Console()
-        logging.basicConfig(format='%(name)s.%(funcName)s() - %(message)s',
-                            handlers=[rich.logging.RichHandler(markup=True, console=console)],
-                            level=getattr(logging, arguments.debug)
-                            )
-
-        logger = logging.getLogger('')
-
-        config = Configuration()
-        # Create SmartRack instance
-        test = SmartRack(console, config.smartrack_servers)
-
-        # Ask user to select rooms
-        test.select_smartrack_rooms(' ATC Room Selection ', 'Please select which rooms you would like to test this library with')
-
-        # Download booked devices
-        test.fetch_booked_devices(' ATC Website Authentication Information ')
-
-        # Retrieve list (no filter, get all devices)
-        result = test.filter()
-
-        # Display all devices in table
-        logger.info('Displaying booked device details')
-        from rich.table import Table
-        table = Table(show_header=True, header_style="bold green", title="Booked Devices", show_lines=True)
-        table.add_column("Room", style="green")
-        table.add_column("Device", style="green")
-        table.add_column("Server", style="cyan")
-        table.add_column("Username", style="cyan")
-        table.add_column("Password", style="red")
-
-        for details in result.values():
-            table.add_row(details['room'], details['fullname'], details['server'], details['username'], details['password'])
-
-        console.print(table)
-
-    except KeyboardInterrupt as err:
-        pass
-    except (Exception,):
-        rich.console.Console().print_exception()
